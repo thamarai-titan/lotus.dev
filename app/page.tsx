e083de104b3934fd8c5ab4dd79f0a25bb19793d4
Thamarai <tmanalan710@gmail.com>
2026-03-02 22:10:33 +0530

    added components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import { Nav } from "./components/layout/Nav";
+import { Footer } from "./components/layout/Footer";
 
 export default function Home() {
   return (
     <div>
-      hello
+      <Nav/>
+      <Footer/>
     </div>
   );
 }
