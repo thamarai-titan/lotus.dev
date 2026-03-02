d0563b43c48b12a57b87b82ad60cb62aef5a3801
Thamarai <tmanalan710@gmail.com>
2026-03-02 22:10:56 +0530

    layout modified

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="antialiased">
+      <body >
         {children}
       </body>
     </html>
