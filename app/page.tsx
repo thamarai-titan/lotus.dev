0f236007cf4095d434ba146da5c4c43ee1b13404
Thamarai <tmanalan710@gmail.com>
2026-03-27 23:24:16 +0530

    page file for main app

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import Image from "next/image";
-import { Nav } from "./components/layout/Nav";
-import { Footer } from "./components/layout/Footer";
+import { Nav } from "@/components/layout/Nav";
 
 export default function Home() {
   return (
-    <div>
-      <Nav/>
-      <Footer/>
-    </div>
+    <main className="w-full min-h-screen flex flex-col items-center justify-start gap-12 p-8">
+      <div className="w-full max-w-7xl flex flex-col items-center justify-center gap-6">
+        <Nav/>
+      </div>
+    </main>
   );
 }
