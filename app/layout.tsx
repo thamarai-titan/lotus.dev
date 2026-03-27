89452567fc162f908ad3de71fd15440eb28ec540
Thamarai <tmanalan710@gmail.com>
2026-03-27 23:23:54 +0530

    geist font added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,22 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Providers } from "./providers";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
+
+const mono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-geist-mono",
+});
 
 
 export const metadata: Metadata = {
   title: "Portfolio",
-  description: "A Personal Portfolio",
+  description: "ilotus.dev a Personal Portfolio",
 };
 
 export default function RootLayout({
@@ -13,9 +25,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body >
-        {children}
+    <html lang="en"
+    className={`${geist.variable} ${mono.variable}`}
+    suppressHydrationWarning>
+      <body suppressHydrationWarning>
+        <Providers>
+          {children}
+          </Providers>     
       </body>
     </html>
   );
